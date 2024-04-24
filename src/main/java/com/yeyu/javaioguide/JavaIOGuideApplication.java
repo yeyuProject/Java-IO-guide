6d051ab5f6ec3501797cd0456a109655f283eea7
陈颢元 <909771863@qq.com>
2024-04-24 18:34:58 +0800

    init project

---
+++ src/main/java/com/yeyu/javaioguide/JavaIOGuideApplication.java
@@ -0,0 +1,17 @@
+package com.yeyu.javaioguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chy
+ * @createDate 2024-04-24 16:11
+ */
+@SpringBootApplication
+public class JavaIOGuideApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(JavaIOGuideApplication.class, args);
+
+    }
+}
